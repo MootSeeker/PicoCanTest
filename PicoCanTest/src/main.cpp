72f452ae8b8b0ed91d86a140581796aa7e96c297
Kevin Perillo <kevin.perillo@processsensing.com>
2023-11-22 12:53:40 +0100

    Add initialization

--- PicoCanTest/src/main.cpp
+++ PicoCanTest/src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
+#include "canbus.h"
 
-
+// Declare can object with 500k Baudrate 
+canbus can( CAN_500K ); 
 
 void setup( ) 
 {
-
+    // Init Serial for debug commands
+    Serial.begin( 115200 ); 
+    // Check if debug interface is initialized without an error
+    while(!Serial); 
 }
 
 void loop() 
