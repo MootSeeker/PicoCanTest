d348d5cf7995c43adb9720bbe3e3b4bebafd362b
mootseeker <mootseeker98@gmail.com>
2023-12-12 15:52:47 +0100

    Add Code to show rgb led basic

--- PicoCanTest/src/main.cpp
+++ PicoCanTest/src/main.cpp
@@ -1,18 +1,76 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_NeoPixel.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN   21
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 1
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) 
+{
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) 
+  {
+    // strip.rainbow() can take a single argument (first pixel hue) or
+    // optionally a few extras: number of rainbow repetitions (default 1),
+    // saturation and value (brightness) (both 0-255, similar to the
+    // ColorHSV() function, default 255), and a true/false flag for whether
+    // to apply gamma correction to provide 'truer' colors (default true).
+    strip.rainbow(firstPixelHue);
+    // Above line is equivalent to:
+    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
+}
+
 
 void setup( ) 
 {
     // Init Serial for debug commands
     Serial.begin( 115200 ); 
-    // Check if debug interface is initialized without an error
-    while( !Serial ); 
 
-    Serial.println(" Pico is working by Visual Studio "); 
+    pinMode( 13, OUTPUT ); 
+    pinMode( 20, OUTPUT); 
+
+    digitalWrite( 20, HIGH ); 
+
+    strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+    strip.show();            // Turn OFF all pixels ASAP
+    strip.setBrightness(25); // Set BRIGHTNESS to about 1/5 (max = 255)
 }
 
 void loop( ) 
 {
-    Serial.println( "Pico is working I don't know! "); 
-    delay( 1000 ); 
+    // Toggle LED to check functionality 
+    digitalWrite( 13, HIGH ); 
+    delay(250); 
+    digitalWrite(13, LOW ); 
+    delay(250); 
+
+
+
+    rainbow(10);             // Flowing rainbow cycle along the whole strip
 }
+
+
+
 
