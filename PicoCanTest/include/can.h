99a7617af7193bf9be8a3f7fdbfef880ad9df055
MootSeeker <mootseeker98@gmail.com>
2024-01-13 17:01:33 +0100

    Working on CAN files :)

--- PicoCanTest/include/can.h
+++ PicoCanTest/include/can.h
@@ -1,4 +1,24 @@
 #ifndef CAN_H
 #define CAN_H
 
+#include <stdint.h>
+
+// CAN Bus class
+class can
+{
+    public: 
+        can( ); 
+        ~can( ); 
+
+        void begin( void ); 
+
+        void getRole( void ); 
+
+    private: 
+    
+
+    protected: 
+
+}; 
+
 #endif
--- PicoCanTest/src/can.c
+++ PicoCanTest/src/can.c
@@ -1,3 +1,27 @@
 #include "can.h"
 
 #include <Arduino.h>
+
+
+
+// Constructor
+can::can( )
+{
+    
+}
+
+can::~can( )
+{
+    // Do nothing here
+}
+
+
+void can::begin( void )
+{
+
+}
+
+void can::getRole( void )
+{
+
+}
